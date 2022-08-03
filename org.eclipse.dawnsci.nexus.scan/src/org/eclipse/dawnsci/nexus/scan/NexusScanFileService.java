58c1a6527926a9769417a7cdab2273b46c6a5c2a
Callum Forrester <callum.forrester@diamond.ac.uk>
2022-08-03 16:31:01 +0100

    DAQ-4232 Remove migrated files from dawnsci
    Change-Id: I3efa30c796dcf2e6359b46a40c6f12b684696f84

--- org.eclipse.dawnsci.nexus.scan/src/org/eclipse/dawnsci/nexus/scan/NexusScanFileService.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.dawnsci.nexus.scan;
-
-import org.eclipse.dawnsci.nexus.NexusException;
-
-public interface NexusScanFileService {
-	
-	public NexusScanFile newNexusScanFile(NexusScanModel nexusScanModel) throws NexusException;
-
-}
